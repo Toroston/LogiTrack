b94bba6987a370f971b4ba47c979342285a00abc
GCampos <gnc@pentaconsulting.com.ar>
2026-03-31 17:40:19 -0300

    Se eliman clases que no se usan mas para testeos

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  roots: ["<rootDir>/src", "<rootDir>/tests"], // 👈 importante en tu caso
-  transform: {
-    "^.+\\.(js|jsx)$": "babel-jest"
-  },
-  moduleFileExtensions: ["js", "jsx"],
-  setupFilesAfterEnv: ["<rootDir>/setupTests.js"]
-};
